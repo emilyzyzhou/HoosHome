20ff331ebed3ed33eb2b1e342dafd9d006d371fe
Christopher Tran <chrisgktran@gmail.com>
2025-11-08 22:36:46 -0500

    trying to get it to run

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
+import "./global.css"
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
