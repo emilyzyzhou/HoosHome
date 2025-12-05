3e00eb31909486546fa2e2c0f6915c9d41873600
emilyzyzhou <csz6wd@virginia.edu>
2025-12-05 03:41:27 -0500

    added necessary files for gcp migration

--- server/src/db/pool.js
+++ server/src/db/pool.js
@@ -1,14 +1,13 @@
 import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
-  socketPath: "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock", // <- XAMPP default on Mac
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,                      // Cloud SQL IP in Cloud Run
+  port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  // waitForConnections: true,
   connectionLimit: 10,
+  namedPlaceholders: true,
 });
 
 async function checkDatabaseConnection() {
